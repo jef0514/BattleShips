205eb63f5fc6287cde393f4a8758d9be4f8798f9
jef0514 <joseph.frankland101@gmail.com>
2021-11-29 14:35:05 +0000

    Added choice of 1 or 2 player

--- src/App.java
+++ src/App.java
@@ -1,8 +1,18 @@
 import tool.*;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Game game = new Game();
-        game.play1P();
+        Scanner s1 = new Scanner(System.in);
+        System.out.println("1 or 2 Players:");
+        int ans = s1.nextInt();
+        if  (ans == 1){
+            game.play1P();
+        }
+        else if (ans == 2) {
+            game.play2P();
+        }
+
     }
 }
